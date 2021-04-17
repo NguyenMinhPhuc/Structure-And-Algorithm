6f94a603e1465f725a7ecd8fdf69b58da80f29ea
Nguyen Minh Phuc <nmphuc@outlook.com>
2021-04-17 16:45:53 +0700

    Add 2021-04-17

---
+++ Project_StructureAndAlgorithm/Projects/Chapter01/Project_0102_Variable/Program.cpp
@@ -0,0 +1,19 @@
+﻿/*Mục tiêu bài tập Variable
+ * 1. Vì sao phải dụng biến
+ * 2. Cách khai báo biến
+ *      2.1. cách khởi tạo biến.
+ *      2.2. gán giá trị cho biến
+ *      2.3. Định nghĩa nhiều biến
+ * 3. Định nghĩa biến ở đâu
+ */
+
+//Thực hành
+//Khai báo thư viện
+#include "iostream"
+using namespace std;
+
+//Phương thức chính của project.
+void main()
+{
+
+}
